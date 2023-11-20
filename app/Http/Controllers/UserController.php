4bd6afa47b16cfa6a7aecf82219ede3b0dfd822e
Isaac Ivan <oscarisaac1504@gmail.com>
2023-11-20 12:04:22 -0600

    Creacion de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\UserRegister;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -13,34 +14,39 @@ class UserController extends Controller
     public function Register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'correo' => 'required',
+            'email' => 'required',
             'pwd' => 'required',
             'nombre' => 'required',
+            'rol' => 'required',
         ]);
+        try {
 
-        $email = DB::table('users')->select('correo')->where('correo', $request->input('correo'))->first();
+            $email = DB::table('users')->select('email')->where('email', $request->input('email'))->first();
 
-        if ($email != null) {
+            if ($email != null) {
+                return response([
+                    'res' => 'false',
+                    'msg' => 'El correo ya se encuentra registrado'
+                ], 200);
+            }
+
+            $data = [
+                'email' => $request->email,
+                'password' => md5($request->pwd),
+                'nombre' => $request->nombre,
+                'rol' => $request->rol
+            ];
+
+            $query = UserRegister::create($data);
+            return response([
+                'res' => 'true',
+                'msg' => 'Cuenta registrada exitosamente'
+            ], 200);
+        } catch (Exception $e) {
             return response([
                 'res' => 'false',
-                'msg' => 'El correo ya se encuentra registrado'
+                'msg' => $e->getMessage(),
             ], 200);
         }
-
-        $data = [
-            'correo' => $request->correo,
-            'password' => md5($request->password),
-            'nombre' => $request->nombre,
-            'rol' => $request->rol
-        ];
-
-
-        $query = UserRegister::create($data);
-
-
-        return response([
-            'res' => 'true',
-            'msg' => 'Cuenta registrada exitosamente'
-        ], 200);
     }
 }
--- app/Models/UserRegister.php
+++ app/Models/UserRegister.php
@@ -10,7 +10,7 @@ class UserRegister extends Model
     use HasFactory;
 
     protected $fillable = [
-        'correo',
+        'email',
         'nombre' ,
         'password',
         'rol'
